eafb182b0d807c8a9605d843de471894ddfe0748
haochang <hackmasker@gmail.com>
2019-11-25 00:20:44 +0800

    fixed

--- php-sys/build.rs
+++ php-sys/build.rs
@@ -6,18 +6,18 @@ use std::path::PathBuf;
 fn main () {
     println!("cargo:rerun-if-env-changed=PHP_LIB_DIR");
     println!("cargo:rerun-if-env-changed=PHP_INCLUDE_DIR");
-    println!("cargo:rerun-if-env-changed=PHP_LINK_STATIC");
+    // println!("cargo:rerun-if-env-changed=PHP_LINK_STATIC");
 
     let default_lib_dir = PathBuf::from("/usr/lib");
     let default_include_dir = PathBuf::from("/usr/include/php");
-    let default_link_static = false;
+    // let default_link_static = false;
 
     let lib_dir = env::var_os("PHP_LIB_DIR").map(PathBuf::from).unwrap_or(default_lib_dir);
     let include_dir = env::var_os("PHP_INCLUDE_DIR").map(PathBuf::from).unwrap_or(default_include_dir);
-    let link_static = match env::var_os("PHP_LINK_STATIC") {
-        Some(val) => val.to_string_lossy().parse::<bool>().unwrap_or(default_link_static),
-        None => default_link_static,
-    };
+    // let link_static = match env::var_os("PHP_LINK_STATIC") {
+    //     Some(val) => val.to_string_lossy().parse::<bool>().unwrap_or(default_link_static),
+    //     None => default_link_static,
+    // };
 
     if !lib_dir.exists() {
         panic!(
@@ -33,13 +33,13 @@ fn main () {
         );
     }
 
-    let link_type = if link_static {
-        "=static"
-    } else {
-        "=dylib"
-    };
+    // let link_type = if link_static {
+    //     "=static"
+    // } else {
+    //     "=dylib"
+    // };
     
-    println!("cargo:rustc-link-lib{}=php", link_type);
+    // println!("cargo:rustc-link-lib{}=php", link_type);
     println!("cargo:rustc-link-search=native={}", lib_dir.to_string_lossy());
 
     let includes = ["/", "/TSRM", "/Zend", "/main"].iter().map(|d| {
@@ -58,10 +58,13 @@ fn main () {
         .whitelist_function("add_index_zval")
         .whitelist_function("add_assoc_zval_ex")
         .whitelist_function("zval_ptr_dtor")
+        .whitelist_function("zend_strpprintf")
         .whitelist_type("zval")
         .whitelist_type("zend_execute_data")
+        .whitelist_type("zend_string")
         .whitelist_type("zend_module_entry")
-        .derive_default(false)
+        .whitelist_type("zend_function_entry")
+        .derive_default(true)
         .header("wrapper.h")
         .generate()
         .expect("Unable to generate bindings");
