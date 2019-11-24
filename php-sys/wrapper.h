58a6733c3cfa5fa7551e19bcaae03735ac2ff033
haochang <hackmasker@gmail.com>
2019-11-24 15:50:00 +0800

    fixed wrapper.h

--- php-sys/wrapper.h
+++ php-sys/wrapper.h
@@ -1,3 +1,8 @@
+#ifndef PHP_RS_WRAPPER_H
+#define PHP_RS_WRAPPER_H
+
+#include <main/php_config.h>
 #include <Zend/zend.h>
+#include <Zend/zend_compile.h>
 #include <main/php.h>
-#include <sapi/embed/php_embed.h>
+#endif
